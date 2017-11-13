6444f24d0daa4e3cd78b90262b71fd93a72ff300
gmmvs <gumamo2daw1617@gmail.com>
2017-11-13 21:06:24 +0100

    prova 2

--- app/src/main/java/com/example/guillem/practica_m08_sopadelletres/BenvingutsActivity.java
+++ app/src/main/java/com/example/guillem/practica_m08_sopadelletres/BenvingutsActivity.java
@@ -1,5 +1,5 @@
 package com.example.guillem.practica_m08_sopadelletres;
-
+//mirar material 4 uf2 android github per veure el ontouch event per fer la seleccio fluïda
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +9,7 @@ public class BenvingutsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_benvinguts);
+
+
     }
 }
