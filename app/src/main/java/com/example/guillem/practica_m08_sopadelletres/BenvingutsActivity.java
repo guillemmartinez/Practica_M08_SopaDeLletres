fc52f0d90d8570c656f232112f5cc8910f9a8e5c
gmmvs <gumamo2daw1617@gmail.com>
2017-11-13 17:41:18 +0100

    prova 2

---
+++ app/src/main/java/com/example/guillem/practica_m08_sopadelletres/BenvingutsActivity.java
@@ -0,0 +1,13 @@
+package com.example.guillem.practica_m08_sopadelletres;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BenvingutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_benvinguts);
+    }
+}
