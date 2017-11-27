e030096e95e620443554090df8cb608f55f4364c
Baro <jbaro.clot@gmail.com>
2017-11-27 19:17:30 +0100

    activity comentari prova afegit

---
+++ app/src/main/java/com/example/guillem/practica_m08_sopadelletres/SopaLletresActivity.java
@@ -0,0 +1,14 @@
+package com.example.guillem.practica_m08_sopadelletres;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SopaLletresActivity extends AppCompatActivity {
+
+    // comentari prova
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sopa_lletres);
+    }
+}
